51cf7ae367d55b21c0502f0e24909f6c611bdcfe
Min Thant Mg <minthant180@gmail.com>
2025-08-02 14:14:23 +0900

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Min Thant on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
